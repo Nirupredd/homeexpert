14f5341a635a9dfcf3527119f8ce80baaa3c8103
Harin Guptha Pasumarthi <saihari799@gmail.com>
2025-03-20 23:13:23 +0530

    hari-20-03

--- server/APIs/worker.js
+++ server/APIs/worker.js
@@ -26,6 +26,22 @@ workerApp.get('/worker/:mobileNumber',expressAsyncHandler(async(req,res)=>{
     res.status(201).send({message:"workers",payload:workers})
 }))
 
+workerApp.put('/workerupdate/:mobileNumber', expressAsyncHandler(async (req, res) => {
+  console.log("Replacing worker:", req.params.mobileNumber);
+  // Find and replace worker by mobile number
+  const updatedWorker = await workerModel.findOneAndReplace(
+      { mobileNumber: req.params.mobileNumber },  // Find worker by mobileNumber
+      req.body,  // Replace with full new object
+      { new: true }  // Return updated worker
+  );
+  if (!updatedWorker) {
+      return res.status(404).send({ message: "Worker not found" });
+  }
+  res.status(200).send({ message: "Worker modified successfully", payload: updatedWorker });
+}));
+
+
+
 //delete worker by id
 workerApp.delete('/workerid/:_id',expressAsyncHandler(async(req,res)=>{
   const d_id=await workerModel.findByIdAndDelete(req.params._id)
@@ -37,5 +53,7 @@ workerApp.delete('/workerph/:mobileNumber',expressAsyncHandler(async(req,res)=>{
    const delete_id=await workerModel.findOneAndDelete({mobileNumber:req.params.mobileNumber})
    res.status(201).send({message:"worker deleted ",payload:delete_id})
 }))
+
+
 
 module.exports=workerApp;
